f4cf530c35064ce5f4bce983094a414fd4e1cebf
anandamb4u <anandamb4u@gmail.com>
2016-01-25 06:53:26 +0000

    addeda contacts page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :posts
+  resources :contacts
   root 'posts#index'
   get '/about'=>'pages#about'
+  #get '/contact'=>'pages#contact'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160106090825) do
+ActiveRecord::Schema.define(version: 20160123142403) do
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.integer  "mobile"
+    t.string   "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
