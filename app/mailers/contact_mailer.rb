d80255ec9d1fd091f48dffdc21557ef82c07aaf3
anandamb4u <anandamb4u@gmail.com>
2016-01-27 04:35:30 +0000

    some more mailer html changes

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,8 @@
 class ContactMailer < ActionMailer::Base
-   default to: "anandamb4u@gmail.com"
    def contact_email(name, email, body)
        @name = name
        @email = email
        @body = body
-       mail(to:"anandamb4u@gmail.com",from:@email,subject: "Message from contact us")
+       mail(to:"anandamb4u@gmail.com",subject: "Message from contact us")
    end
 end
